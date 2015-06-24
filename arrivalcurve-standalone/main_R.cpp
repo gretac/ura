e1f2eefc6e14c4b524938c613f0592ebf73e0382
Sebastian Fischmeister <sfischme@uwaterloo.ca>
2015-06-24 08:16:26 -0400

    also return the count

--- arrivalcurve-standalone/main_R.cpp
+++ arrivalcurve-standalone/main_R.cpp
@@ -11,15 +11,16 @@ Rcpp::List computer_arrival_curve_rwrapper(Rcpp::NumericVector e, float max_even
 
    vector<float> max_events, max_left, max_right, min_events, min_left, min_right;
 
-   compute_arrival_curve(ex, min_window_size, max_window_size, max_event, max_events, max_left, max_right, min_events, min_left, min_right);
+   float count = compute_arrival_curve(ex, min_window_size, max_window_size, max_event, max_events, max_left, max_right, min_events, min_left, min_right);
 
    return Rcpp::List::create (
                               Rcpp::Named("max_events") = max_events,
-                              Rcpp::Named("max_left") = max_left,
-                              Rcpp::Named("max_right") = max_right,
+                              Rcpp::Named("max_left")   = max_left,
+                              Rcpp::Named("max_right")  = max_right,
 
                               Rcpp::Named("min_events") = min_events,
-                              Rcpp::Named("min_left") = min_left,
-                              Rcpp::Named("min_right") = min_right
+                              Rcpp::Named("min_left")   = min_left,
+                              Rcpp::Named("min_right")  = min_right,
+                              Rcpp::Named("count")      = count
                               );
 }
